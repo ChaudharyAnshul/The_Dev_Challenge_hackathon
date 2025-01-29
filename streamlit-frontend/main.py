3fe527b662c75fd4e29b2912b4266899efbb9ec4
Muskan raisinghani <raisinghani.m@northeastern.edu>
2025-01-29 00:12:43 -0500

    Upload file and get job_match pages added

--- streamlit-frontend/main.py
+++ streamlit-frontend/main.py
@@ -3,18 +3,24 @@
 import streamlit as st
 from components.login_page import login_page
 from components.signup_page import signup_page
+import components.upload_page as upload_page
+import components.get_job_matches as get_job_matches
 from PIL import Image
 
 # st.set_page_config(layout="wide")
 
+PAGES = {
+    "Upload Files": upload_page,
+    "Find Jobs": get_job_matches
+}
 
 def main():
-    st.set_page_config(page_title="Job Recommendation System")
+    st.set_page_config(page_title="Job Match")
     if 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = False
     
     if not st.session_state['logged_in']:      
-        st.title("Job Recommendation System")
+        st.title("Job Match")
         st.title("Login/Signup")
         tab1, tab2 = st.tabs(["Login", "Signup"])
         
@@ -25,11 +31,18 @@ def main():
             signup_page()
     else:
         st.sidebar.title('Menu')
+        selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 
         if st.sidebar.button('Logout'):
             st.session_state['logged_in'] = False
             del st.session_state['access_token']
             st.rerun()
+
+        if st.session_state['logged_in']:
+            page = PAGES[selection]
+            page_function = getattr(
+                page, 'show_' + selection.lower().replace(' ', '_'))
+            page_function()
 
 if __name__ == "__main__":
     main()
